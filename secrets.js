2b430d93e61f519ffb3715794a0b0ca9e6e49502
Shubham Pathak <sam76870@gmail.com>
2021-10-23 19:19:57 +0530

    add secret

---
+++ secrets.js
@@ -0,0 +1,8 @@
+// let DB_LINK = "mongodb+srv://admin:oiQPUaCcBXyI2Btb@cluster0.fzqgp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+module.exports = {
+    PASSWORD: "oiQPUaCcBXyI2Btb",
+    JWT_KEY: "aweeifhwlfhzsflweah",
+    APP_PASSWORD: "nbuamomssznshvwr",
+    KEY_ID: "rzp_test_g3cpvfOCs2b6ro",
+    KEY_SECRET: "B3dU9tpwP1bOBqG295Ki21oi"
+};
